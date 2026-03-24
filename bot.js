a939f22d268efd8829c4874f0fcfa8bf50538da7
Enim4028 <54321424+Enim4028@users.noreply.github.com>
2026-03-24 20:31:37 +0100

    Add Express server to bot.js

--- bot.js
+++ bot.js
@@ -24,3 +24,15 @@ function startBot() {
 }
 
 startBot();
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot attivo');
+});
+
+app.listen(3000, () => {
+  console.log('Web server attivo');
+});
